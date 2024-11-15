e8a7bbed9a285dea0ea7d16d34648f0822490a3f
Charlie Nunn <113851328+CharlieDoll@users.noreply.github.com>
2024-11-15 11:56:36 +0000

    Add files via upload
    joke api added

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('about/', views.about_view, name='about'),
     path('contact/', views.contact_view, name='contact'),
+    path('api-display/', views.display_api_data, name='display_api_data'),
 ]
 
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,8 @@
+import requests
 from django.shortcuts import render
-
 from .models import Post
+
+
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'post_list.html', {'posts': posts})
@@ -10,3 +12,10 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, 'contact.html')
+
+def display_api_data(request):
+    url = "https://v2.jokeapi.dev/joke/Any?safe-mode"
+    response = requests.get(url)
+    data = response.json()if response.status_code == 200 else {}
+    print(data)
+    return render(request, 'api_display/display.html', {'data': data})
